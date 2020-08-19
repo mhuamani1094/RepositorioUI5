5682707980bd71e8ce810a3c26a153dd8622d81f
mhuamani1094 <mhuamani.1094@gmail.com>
2020-08-19 07:08:06 +0000

    My First Commit

---
+++ MyFirstProject/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("MyFirstProject.MyFirstProject.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
